40f6433adfdacc3385b9b139c7ea737e9a1143a2
Solx <solx@Solxs-MacBook-Pro.local>
2022-05-03 14:39:48 +0700

    modify env

--- template/app/screens/Home/index.js
+++ template/app/screens/Home/index.js
@@ -2,12 +2,17 @@ import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
 import { observer } from "mobx-react-lite"
 import SplashScreen from 'react-native-splash-screen'
-
+import { useStores } from '../../models';
 const HomeScreen = observer(({
 
 }) => {
+    const { demoStore } = useStores()
+    const { demo } = demoStore
     useEffect(() => {
         SplashScreen.hide();
+        console.log('====================================');
+        console.log("demoStore", demoStore);
+        console.log('====================================');
     }, [])
     return (
         <View style={{ justifyContent: "center", alignContent: "center", alignItems: "center", flex: 1 }}>
